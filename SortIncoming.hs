e86ceb6c4a94a88fe025d85006647edebe0552b5
Trevor Caira <trevor@caira.com>
2012-06-01 17:27:13 -0400

    Updated for GHC 7.2

--- SortIncoming.hs
+++ SortIncoming.hs
@@ -3,29 +3,25 @@ module Main where
 import Control.Applicative ((<$>), (<*), (<*>))
 import Control.Monad       (filterM, forM_, unless, when)
 import Data.Char           (toLower)
-import Data.Function       (on)
 import Data.List           (delete, inits, intercalate, nub)
-import Data.Text.Encoding  (encodeUtf8, decodeUtf8)
 import Text.Printf         (printf)
 import System.FilePath     (dropFileName, joinPath, splitDirectories,
                             takeExtension)
-import System.Directory    (createDirectory, doesDirectoryExist,
-                            doesFileExist, renameFile)
 import System.Environment  (getArgs, getProgName)
 import System.Exit         (ExitCode(..), exitFailure)
 import System.FilePath     ((</>), combine)
+import System.Directory    (createDirectory, doesDirectoryExist,
+                            doesFileExist, renameFile)
 import System.IO           (hPutStrLn, hPutStr, hFlush, stdout,
                             stderr)
 import System.Process      (readProcessWithExitCode)
 import System.Console.GetOpt
-import qualified Data.ByteString.Char8 as C
 import qualified Data.Map as M
 
 import System.FilePath.Find (always, extension, find)
 import Text.Parsec          (Parsec, runParser, many,
                              many1, getState, modifyState, noneOf,
                              oneOf, char, newline, sepEndBy1)
-import qualified Data.Text as T
 
 data Opts = Opts
     { assumeYes         :: Bool
@@ -71,31 +67,21 @@ parseOpts argv =
       (opts, _, [])  -> return $ foldl (flip id) optionDefaults opts
       (_,    _, e:_) -> hPutStr stderr e >> usage
 
-fromUTF8 :: String -> String
-fromUTF8 = T.unpack . decodeUtf8 . C.pack
-
-toUTF8 :: String -> String
-toUTF8 = C.unpack . encodeUtf8 . T.pack
-
 findFiles :: FilePath -> IO [FilePath]
-findFiles root = map fromUTF8 <$> find always isSourceExtension root
+findFiles root = find always isSourceExtension root
     where isSourceExtension = flip elem sourceExtensions <$> extension
           sourceExtensions = [".ogg", ".flac"]
 
-renameFile' :: FilePath -> FilePath -> IO ()
-renameFile' = renameFile `on` toUTF8
+maybeToEither :: e -> Maybe a -> Either e a
+maybeToEither e = maybe (Left e) Right
 
-createDirectory' :: FilePath -> IO ()
-createDirectory' = createDirectory . toUTF8
-
-doesFileExist' :: FilePath -> IO Bool
-doesFileExist' = doesFileExist . toUTF8
-
-doesDirectoryExist' :: FilePath -> IO Bool
-doesDirectoryExist' = doesDirectoryExist . toUTF8
+maybeRead :: (Read a) => String -> Maybe a
+maybeRead s = case reads s of
+                [(x, "")] -> Just x
+                _         -> Nothing
 
 (!) :: Ord k => M.Map k a -> k -> Either k a
-a ! k = maybe (Left k) Right (M.lookup k a)
+a ! k = maybeToEither k (M.lookup k a)
 
 metadata :: String
          -> StringMap
@@ -104,14 +90,16 @@ metadata ext m = do
   album <- m ! "album"
   title <- m ! "title"
   artist <- m ! "artist"
-  track <- read <$> m ! "tracknumber"
-  when (track < 1) $ Left "non-positive track"
+  track <- m ! "tracknumber"
+  let errMsg = "tracknumber did not parse: " ++ track
+  trackNumber <- maybeToEither errMsg $ maybeRead track
+  when (trackNumber < 1) $ Left "non-positive track"
   return Metadata
              { metaExtension = ext
              , metaAlbum = album
              , metaTitle = title
              , metaArtist = artist
-             , metaTrack = track
+             , metaTrack = trackNumber
              }
 
 commentParser :: Parsec String StringMap StringMap
@@ -123,7 +111,7 @@ commentParser = many (lineParser `sepEndBy1` newline) >> getState
 
 runCommand :: String -> [String] -> IO String
 runCommand cmd args = do
-  res <- readProcessWithExitCode cmd (map toUTF8 args) ""
+  res <- readProcessWithExitCode cmd args ""
   case res of
     (ExitSuccess, out, _)   -> return out
     (_,           _,   err) -> error err
@@ -138,7 +126,7 @@ addMetadata path = either metadataError id .
                             ["--export-tags-to=-", path]
           getMeta ".ogg"  = runCommand "vorbiscomment" ["-e", path]
           getMeta _       = error $ "unhandled extension " ++ ext
-          metadataError e = error $ toUTF8 path ++ ": " ++ e
+          metadataError e = error $ path ++ ": " ++ e
 
 getOutputPath :: Integer -> Metadata -> FilePath
 getOutputPath lastTrack meta =
@@ -156,33 +144,34 @@ extractDirs = drop 1 . map joinPath . inits . splitDirectories .
 
 prompt :: [FilePath] -> [(FilePath, FilePath)] -> IO Bool
 prompt newDirs txcodes = do
-  putStrLn "Creating..."
+  putStrLn "Creating"
+  putStrLn "========"
   forM_ newDirs $ \ d -> putStrLn $ " * " ++ d
-  putStrLn "Moving..."
+  putStrLn "\nMoving"
+  putStrLn "======"
   forM_ txcodes $ \(x, y) -> putStrLn $ " * " ++ x ++ " -> " ++ y
-  putStr "Continue [y/N]? "
+  putStr "\nContinue [y/N]? "
   hFlush stdout
   flip elem ["Y", "y"] <$> getLine
 
 main :: IO ()
 main = do
-  args <- getArgs
   Opts { assumeYes = yes
        , incomingDirectory = incoming
        , mastersDirectory  = masters
-       } <- parseOpts args
+       } <- parseOpts =<< getArgs
   sourceFiles <- findFiles incoming
   annotated <- mapM addMetadata sourceFiles
   let lastTrack = maximum (map metaTrack annotated)
       destFiles = map ((masters </>) . getOutputPath lastTrack) annotated
       destDirs = nub $ concatMap extractDirs destFiles
       toMove = zip sourceFiles destFiles
-  wouldOverwrite <- filterM doesFileExist' destFiles
+  wouldOverwrite <- filterM doesFileExist destFiles
   unless (null wouldOverwrite) $
        error ("refusing to overwrite target files (would overwrite " ++
-              intercalate ", " (map toUTF8 wouldOverwrite) ++ ")")
-  missing <- filterM (fmap not . doesDirectoryExist') destDirs
+              intercalate ", " wouldOverwrite ++ ")")
+  missing <- filterM (fmap not . doesDirectoryExist) destDirs
   continue <- if yes then return True else prompt missing toMove
   when continue $ do
-         mapM_ createDirectory' missing
-         mapM_ (uncurry renameFile') toMove
+         mapM_ createDirectory missing
+         mapM_ (uncurry renameFile) toMove
